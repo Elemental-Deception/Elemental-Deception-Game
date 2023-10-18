450f8aa76df2c457c948e88a4bd4c72cc03840a3
Im-Logan-Lol <145610851+Im-Logan-Lol@users.noreply.github.com>
2023-10-18 01:53:55 -0400

    Changed a lot

--- Assets/Script/CharacterStatLogic.cs
+++ Assets/Script/CharacterStatLogic.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CharacterStatLogic: MonoBehaviour
+public class CharacterStatLogic : MonoBehaviour
 {
     public Image healthBar;
-    private float health = 100f;
-    private float maxHealth = 100f;
+    private class CharacterStats
+    {
+        public float Health { get; set; } = 100;
+        public float MaxHealth { get; set; } = 100;
+        public float Mana { get; set; } = 100;
+        public float MaxMana { get; set; } = 100;
+        public float XP { get; set; } = 0;
+    }
+
+    private CharacterStats characterStats = new CharacterStats();
     private Animator animator;
     private bool isDead = false;
-    public float DelayAnimation;
 
     public void Start()
     {
@@ -19,9 +26,9 @@ public class CharacterStatLogic: MonoBehaviour
 
     public void TakeDmg(int damage)
     {
-        health -= damage;
+        characterStats.Health -= damage;
         animator.SetTrigger("Hurt");
-        if (health <= 0 && !isDead)
+        if (characterStats.Health <= 0 && !isDead)
         {
             healthBar.fillAmount = 0f;
             animator.SetTrigger("Dead");  // Assuming you have a trigger named "Die" to play death animation
@@ -30,13 +37,34 @@ public class CharacterStatLogic: MonoBehaviour
         }
         else
         {
-            healthBar.fillAmount = health / maxHealth;
+            healthBar.fillAmount = characterStats.Health / characterStats.MaxHealth;
         }
     }
 
-    public float getHealth()
+    public void SpendMana(int manaCost)
     {
-        return health;
+        if (characterStats.Mana - manaCost >= 0 && !isDead)
+        {
+            characterStats.Mana -= manaCost;
+        }
+        else
+        {
+            Debug.Log("Not Enough Mana!");
+        }
     }
 
+    public float GetHealth()
+    {
+        return characterStats.Health;
+    }
+
+    public float GetMana()
+    {
+        return characterStats.Mana;
+    }
+
+    public float GetXP()
+    {
+        return characterStats.XP;
+    }
 }
