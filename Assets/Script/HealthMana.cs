d8ebace60089dcde54abafd2cc34fb066e42a52e
Karan Patel <karanpatel7301@gmail.com>
2023-12-01 19:20:31 -0500

    hearts and mana

---
+++ Assets/Script/HealthMana.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMana : MonoBehaviour
+{
+     public GameObject player;
+
+     public Sprite heartSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
